91acac6957f3cc29ef995914ef0d03feb612f704
Mert Karahan <mertkarahan955@gmail.com>
2025-11-25 23:15:15 +0300

    full reproduction v1

--- download_datasets.py
+++ download_datasets.py
@@ -1,3 +1,2 @@
 from ogb.nodeproppred import NodePropPredDataset
-d = NodePropPredDataset(name='ogbn-products', root='./datasets')
-print('products downloaded OK')
+d = NodePropPredDataset(name='ogbn-products', root='./datasets', download=False)
